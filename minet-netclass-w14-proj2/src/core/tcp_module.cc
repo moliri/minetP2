0a2e4ba7ac9ecc6d647c723dbfd4448df98ae51f
Jordan Geltner <jordangeltner2014@u.northwestern.edu>
2014-02-19 21:22:33 -0600

    matched tcp_module 'if handle=mux' section to udp_module

--- minet-netclass-w14-proj2/src/core/tcp_module.cc
+++ minet-netclass-w14-proj2/src/core/tcp_module.cc
@@ -10,7 +10,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
+#include <string.h>
 #include <iostream>
 
 #include "Minet.h"
@@ -21,9 +21,19 @@ using std::endl;
 using std::cerr;
 using std::string;
 
+struct TCPState {
+  std::ostream & Print(std::ostream &os) const { os << "TCPState()"; return os;}
+
+  friend std::ostream &operator<<(std::ostream &os, const TCPState& L) {
+		return L.Print(os);
+  }
+};
+
 int main(int argc, char *argv[])
 {
   MinetHandle mux, sock;
+
+  ConnectionList<TCPState> clist;
 
   MinetInit(MINET_TCP_MODULE);
 
@@ -31,7 +41,7 @@ int main(int argc, char *argv[])
   sock=MinetIsModuleInConfig(MINET_SOCK_MODULE) ? MinetAccept(MINET_SOCK_MODULE) : MINET_NOHANDLE;
 
   if (MinetIsModuleInConfig(MINET_IP_MUX) && mux==MINET_NOHANDLE) {
-    MinetSendToMonitor(MinetMonitoringEvent("Can't connect to mux"));
+    MinetSendToMonitor(MinetMonitoringEvent("Can't connect to ip_mux"));
     return -1;
   }
 
@@ -40,7 +50,9 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic"));
+  cerr << "tcp_module handling tcp traffic.......\n";
+
+  MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic........"));
 
   MinetEvent event;
 
@@ -54,8 +66,51 @@ int main(int argc, char *argv[])
       //  Data from the IP layer below  //
       if (event.handle==mux) {
 	Packet p;
+	unsigned short len;
+	bool checksumok;
 	MinetReceive(mux,p);
-	unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
+	p.ExtractHeaderFromPayload<TCPHeader>(8);
+	TCPHeader tcph;
+	tcph=p.FindHeader(Headers::TCPHeader);
+	checksumok=tcph.IsCorrectChecksum(p);
+	IPHeader iph;
+	iph=p.FindHeader(Headers::IPHeader);
+	Connection c;
+	//upd_module says that "source" is interpreted as "this machine"
+	//so this should be flipped
+	iph.GetDestIP(c.src);
+	iph.GetSourceIP(c.dest);
+
+	iph.GetProtocol(c.protocol);
+
+	tcph.GetDestPort(c.srcport);
+	tcph.GetSourcePort(c.destport);
+
+	ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+	if (cs!=clist.end()) {
+	  len= TCPHeader::EstimateTCPHeaderLength(p);
+	  cerr << "estimated header len="<<len<<"\n";
+	  Buffer &data = p.GetPayload().ExtractFront(len);
+	  SockRequestResponse write(WRITE, (*cs).connection, data, len, EOK);
+	  
+	  if (!checksumok) {
+		MinetSendToMonitor(MinetMonitoringEvent("forwarding packet to sock even though checksum failed"));
+		}
+		MinetSend(sock,write);
+	} else {
+		MinetSendToMonitor(MinetMonitoringEvent("Unknown port, sending ICMP error message"));
+		IPAddress source;
+		iph.GetSourceIP(source);
+		ICMPPacket error(source,DESTINATION_UNREACHABLE,PORT_UNREACHABLE,p);
+		MinetSendToMonitor(MinetMonitoringEvent("ICMP error message has been sent to hose"));
+		MinetSend(mux, error);
+	}
+	cerr << "TCP Packet: IP Header is "<<iph<<" and ";
+	cerr << "TCP Header is "<<tcph << " and ";
+	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+	}
+
+/*	unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
 	cerr << "estimated header len="<<tcphlen<<"\n";
 	p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
 	IPHeader ipl=p.FindHeader(Headers::IPHeader);
@@ -63,10 +118,9 @@ int main(int argc, char *argv[])
 
 	cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
 	cerr << "TCP Header is "<<tcph << " and ";
-
 	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
 	
-      }
+      }*/
       //  Data from the Sockets layer above  //
       if (event.handle==sock) {
 	SockRequestResponse s;
