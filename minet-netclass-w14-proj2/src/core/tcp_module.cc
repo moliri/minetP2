5a2100e68f99acf5d4c31b3e1aa623297bf206d5
Jordan Geltner <jordangeltner2014@u.northwestern.edu>
2014-02-24 15:53:04 -0600

    modified much of event_handle==sock section. making the packet, getting the headers. also completed (not sure if it works) CONNECT and ACCEPT cases

--- minet-netclass-w14-proj2/src/core/tcp_module.cc
+++ minet-netclass-w14-proj2/src/core/tcp_module.cc
@@ -18,8 +18,15 @@
 
 using namespace std;
 
+void die(int fd) {
+    minet_close(fd);
+    minet_deinit();
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
+  //i don't know if we should change these at all--jg
   MinetHandle mux, sock;
 
   ConnectionList<TCPState> clist;
@@ -54,133 +61,211 @@ int main(int argc, char *argv[])
     } else {
       //  Data from the IP layer below  //
       if (event.handle==mux) {
-	Packet p;
-	bool checksumok;
-	MinetReceive(mux,p);
-	//GET TCP HEADER SIZE
-	//p.ExtractHeaderFromPayload<TCPHeader>(HEADER SIZE);
-	TCPHeader tcph;
-	tcph=p.FindHeader(Headers::TCPHeader);
-	checksumok=tcph.IsCorrectChecksum(p);
-	IPHeader iph;
-	iph=p.FindHeader(Headers::IPHeader);
-	Connection c;
-	//upd_module says that "source" is interpreted as "this machine"
-	//so this should be flipped
-	iph.GetDestIP(c.src);
-	iph.GetSourceIP(c.dest);
+		Packet p;
+		bool checksumok;
+		MinetReceive(mux,p);
+		//GET TCP HEADER SIZE-jg
+		//p.ExtractHeaderFromPayload<TCPHeader>(HEADER SIZE);
+		TCPHeader tcph;
+		tcph=p.FindHeader(Headers::TCPHeader);
+		checksumok=tcph.IsCorrectChecksum(p);
+		IPHeader iph;
+		iph=p.FindHeader(Headers::IPHeader);
+		Connection c;
+		//upd_module says that "source" is interpreted as "this machine"
+		//so this should be flipped
+		iph.GetDestIP(c.src);
+		iph.GetSourceIP(c.dest);
 
-	iph.GetProtocol(c.protocol);
+		iph.GetProtocol(c.protocol);
 
-	tcph.GetDestPort(c.srcport);
-	tcph.GetSourcePort(c.destport);
+		tcph.GetDestPort(c.srcport);
+		tcph.GetSourcePort(c.destport);
 
-	//match ports and ips to existing connection
-	//if no existing match, check flag for syn bit
-	//if syn, make new connection, send syn ack
-	//lookup ctags
-	ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
-	if (cs!=clist.end()) {
-		switch(cs->state.GetState()){
-			case CLOSED:
-				cerr << "Connection is closed...\n";
-				return -1;
-				break;
-			case LISTEN:
-					//put the seq num with connection, so
-					//next packet is in order
-					//send syn
-					//send syn ack
-					//change state to syn_rcvd	
-				break;
-			case SYN_RCVD:
-			case SYN_SENT:
-			case SYN_SENT1:
-			case ESTABLISHED:
-			case SEND_DATA:
-			case CLOSE_WAIT:
-			case FIN_WAIT1:
-			case CLOSING:
-			case LAST_ACK:
-			case FIN_WAIT2:
-			case TIME_WAIT:
-			default:
-				cerr << "not implemented...\n";
-				break;
+		//match ports and ips to existing connection
+		//if no existing match, check flag for syn bit
+		//if syn, make new connection, send syn ack
+		//lookup ctags-jg
+		ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+		if (cs!=clist.end()) {
+			cerr << "State is " << cs->state.GetState();
+			cerr << endl;
+			switch(cs->state.GetState()){
+				case CLOSED:
+					cerr << "Connection is closed...\n";
+					return -1;
+					break;
+				case LISTEN:
+						//put the seq num with connection, so
+						//next packet is in order
+						//send syn
+						//send syn ack
+						//change state to syn_rcvd	-jg
+					break;
+				case SYN_RCVD:
+				case SYN_SENT:
+				case SYN_SENT1:
+				case ESTABLISHED:
+				case SEND_DATA:
+				case CLOSE_WAIT:
+				case FIN_WAIT1:
+				case CLOSING:
+				case LAST_ACK:
+				case FIN_WAIT2:
+				case TIME_WAIT:
+				default:
+					cerr << "not implemented...\n";
+					break;
+			}
 		}
-	}
-	  /*len= TCPHeader::EstimateTCPHeaderLength(p);
-	  cerr << "estimated header len="<<len<<"\n";
-	  Buffer &data = p.GetPayload().ExtractFront(len);
-	  SockRequestResponse write(WRITE, (*cs).connection, data, len, EOK);
-	  
-	  if (!checksumok) {
-		MinetSendToMonitor(MinetMonitoringEvent("forwarding packet to sock even though checksum failed"));
+		  /*len= TCPHeader::EstimateTCPHeaderLength(p);
+		  cerr << "estimated header len="<<len<<"\n";
+		  Buffer &data = p.GetPayload().ExtractFront(len);
+		  SockRequestResponse write(WRITE, (*cs).connection, data, len, EOK);
+		  
+		  if (!checksumok) {
+			MinetSendToMonitor(MinetMonitoringEvent("forwarding packet to sock even though checksum failed"));
+			}
+			MinetSend(sock,write);
+		} else {
+			MinetSendToMonitor(MinetMonitoringEvent("Unknown port, sending ICMP error message"));
+			IPAddress source;
+			iph.GetSourceIP(source);
+			ICMPPacket error(source,DESTINATION_UNREACHABLE,PORT_UNREACHABLE,p);
+			MinetSendToMonitor(MinetMonitoringEvent("ICMP error message has been sent to hose"));
+			MinetSend(mux, error);
 		}
-		MinetSend(sock,write);
-	} else {
-		MinetSendToMonitor(MinetMonitoringEvent("Unknown port, sending ICMP error message"));
-		IPAddress source;
-		iph.GetSourceIP(source);
-		ICMPPacket error(source,DESTINATION_UNREACHABLE,PORT_UNREACHABLE,p);
-		MinetSendToMonitor(MinetMonitoringEvent("ICMP error message has been sent to hose"));
-		MinetSend(mux, error);
-	}
-	cerr << "TCP Packet: IP Header is "<<iph<<" and ";
-	cerr << "TCP Header is "<<tcph << " and ";
-	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
-	
-	//we have the tcp headers, so we should see what kind
-	//of packet we have, then compare it to current connections
-	//get the flags so we can test them
-	
+		cerr << "TCP Packet: IP Header is "<<iph<<" and ";
+		cerr << "TCP Header is "<<tcph << " and ";
+		cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+		
+		//we have the tcp headers, so we should see what kind
+		//of packet we have, then compare it to current connections
+		//get the flags so we can test them
+		
 
-	char * f;
-	tcph.GET_FLAGS(f);
-	
+		char * f;
+		tcph.GET_FLAGS(f);
+		
 
-	}*/
+		}*///-jg
 
 
-/*	unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
-	cerr << "estimated header len="<<tcphlen<<"\n";
-	p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
-	IPHeader ipl=p.FindHeader(Headers::IPHeader);
-	TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
+	/*	unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
+		cerr << "estimated header len="<<tcphlen<<"\n";
+		p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
+		IPHeader ipl=p.FindHeader(Headers::IPHeader);
+		TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
 
-	cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
-	cerr << "TCP Header is "<<tcph << " and ";
-	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
-	
-      }*/
-      //  Data from the Sockets layer above  //
-	  }
-      if (event.handle==sock) {
-	SockRequestResponse request;
-	if(MinetReceive(sock,request)< 0){
-		cerr << "Unable to receive Socket...\n";
-		return -1;
-	}
-	switch (request.type) {
-	  case CONNECT:
-	  //creat socket and send syn packet to IP layer to establish
-	  //change state to syn-sent
-	  case ACCEPT:
-	  //create a connection and put into clist
-	  case STATUS:
-	  case WRITE:
-	  //send out the packet to the designated connection in clist
-	  case FORWARD:
-	  case CLOSE:
-	  //send fin bit 
-	  //change state
-	  default:
-		SockRequestResponse reply;
-		reply.type=STATUS;
-		reply.error=EWHAT;
-		MinetSend(sock,reply);
+		cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
+		cerr << "TCP Header is "<<tcph << " and ";
+		cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+		
+		  }*/
+		  //  Data from the Sockets layer above  //
+		  }
+    if (event.handle==sock) {
+		SockRequestResponse request;
+		if(MinetReceive(sock,request)< 0){
+			cerr << "Unable to receive Socket...\n";
+			return -1;
+		}
+		  Packet p;
+		  //following lines copied from lines 127-137 of udp_module.cc-jg
+		  unsigned bytes = MIN_MACRO(TCP_HEADER_OPTION_MAX_LENGTH, request.data.GetSize());
+		  Packet p(request.data.ExtractFront(bytes));
 
-	cerr << "Received Socket Request:" << request << endl;
+		  len = request.data.GetSize();//idea from buffer.h to get tcpheader size-jg
+		  
+		  //make IPHeader first-jg
+		  IPHeader ih;
+		  ih.SetProtocol(IP_PROTO_TCP);
+		  ih.SetSourceIP(request.connection.src);
+		  ih.SetDestIP(request.connection.dest);
+		  ih.SetTotalLength(bytes+len+IP_HEADER_BASE_LENGTH);
+		  // push it onto the packet
+		  p.PushFrontHeader(ih);
+		switch (request.type) {
+		  case CONNECT:
+		  {
+			  //create socket-jg
+			  sock = minet_socket(SOCK_STREAM);
+			  if (sock < 0) {
+				cerr << "Can't create socket." << endl;
+				minet_perror("reason:");
+				die(sock);
+			  }
+			  cerr << "Socket Created\n";
+			  //send syn packet to IP layer to establish connection -jg
+			    
+				//now make TCPHeader-jg
+			  TCPHeader th;
+			  th.SetSourcePort(request.connection.srcport,p);
+			  th.SetDestPort(request.connection.destport,p);
+			  th.SetLength(len+bytes,p);
+			  
+			  //set the syn bit-jg
+			  char f = 0;
+			  th.SET_SYN(f);
+			  
+			  //push it onto the packet-jg
+			  p.PushFrontHeader(th);
+			  
+			  //send packet to network layer-jg
+			  MinetSend(mux,p);
+			  
+			  //make reply to send to socket-jg
+			  SockRequestResponse reply;
+			  reply.type=CONNECT;
+			  reply.connection=request.connection;
+			  reply.bytes=bytes;
+			  reply.error=EOK;
+			  MinetSend(sock,reply);
+			  
+			  //change state to syn-sent-jg
+			  cs->state.SetState(SYN_SENT);
+			break;
+			}
+		  case ACCEPT:
+		  //create a connection and put into clist-jg
+		  ConnectionToStateMapping<TCPState> mapping;
+		  mapping.connection=request.connection;
+		  
+		  //before adding, check if there is already this connection-jg
+		  ConnectionList<TCPState>::iterator cs = clist.FindMatching(request.connection);
+		  if (cs!=clist.end()){
+				//if there is a connection, we can't accept one that is here-jg
+				SockRequestResponse reply;
+				reply.type=STATUS;
+				reply.error=ERESOURCE_UNAVAIL;
+				MinetSend(sock,reply);
+				break;
+		  }
+		  //add the connection to the list.-jg
+		  clist.push_back(m);
+		
+			SockRequestResponse reply;
+			reply.type=ACCEPT;
+			reply.connection=request.connection;
+			reply.bytes=bytes;
+			reply.error=EOK;
+			MinetSend(sock,reply);
+			
+			break;
+		  case STATUS:
+		  case WRITE:
+		  //send out the packet to the designated connection in clist-jg
+		  case FORWARD:
+		  case CLOSE:
+		  //send fin bit -jg
+		  //change state-jg
+		  default:
+			SockRequestResponse reply;
+			reply.type=STATUS;
+			reply.error=EWHAT;
+			MinetSend(sock,reply);
+
+			cerr << "Received Socket Request:" << request << endl;
         }
       }
     }
